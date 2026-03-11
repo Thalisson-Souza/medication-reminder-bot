05f7615553dabc3d8e511c0001cd3d0e431152fb
Thalisson-Souza <thalisson.souza@acad.ufsm.br>
2026-03-11 00:19:58 -0300

    refactor: restructure whatsapp client

--- src/api.js
+++ src/api.js
@@ -1,21 +1,4 @@
-import express from "express";
-import { Client } from "whatsapp-web.js";
-import qrcode from "qrcode-terminal";
-
-const client = new Client();
-
-client.once("ready", () => {
-  console.log("cliente is ready");
-});
-
-client.on("qr", (qr) => {
-  qrcode.generate(qr, { small: true });
-});
+import client from "./config/whatsapp.js";
+import "./handlers/whatsapp.handler.js";
 
 client.initialize();
-
-const app = express();
-
-app.listen(3000, () => {
-  console.log("server is running localhost:3000");
-});
